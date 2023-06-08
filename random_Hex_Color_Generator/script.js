25e150775703f3ac7cadd071fe6faa0719bd3633
Roger Acosta <rogeracosta359@gmail.com>
2023-06-08 18:04:42 -0500

    new project random_Hex_Color

---
+++ random_Hex_Color_Generator/script.js
@@ -0,0 +1,19 @@
+const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate')
+
+const generateColor = () => {
+  //generate the random chex color
+const randomColor = Math.random().toString(16).substring(2,8);
+ // change the background to the color generated
+ document.body.style.backgroundColor = "#" + randomColor;
+ hex.innerHTML = "#" + randomColor;
+
+}
+
+btn.addEventListener('click', generateColor)
+
+generateColor()
+// let color = Math.random()
+// color = Math.random().toString(16); 
+// color = Math.random().toString(16).substring(2, 8); 
+// console.log(color)
