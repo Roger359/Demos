d3a5867518e772c08e6ee254cb0f0bd7c8ef110d
Roger Acosta <rogeracosta359@gmail.com>
2023-06-19 20:18:15 -0500

    dad joke generator project

---
+++ 14.DadJokeGenerator/script.js
@@ -0,0 +1,40 @@
+
+
+const btn = document.querySelector(".btn");
+const jokeEl = document.getElementById("joke");
+
+
+const apiKey = config.MY_KEY
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+
+const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+async function getJoke() {
+  try {
+  
+    // before fetch
+    jokeEl.innerText = "Updating...";
+    btn.disabled = true;
+    btn.innerText = "LOADING...";
+
+    const response = await fetch(apiUrl, options);
+    const data = await response.json();
+
+    // after fetch
+    jokeEl.innerText = data[0].joke;
+    btn.disabled = false;
+    btn.innerText = "TELL ME A JOKE";
+  } catch (error) {
+    btn.disabled = true;
+    btn.innerText = "TELL ME A JOKE";
+    jokeEl.innerText = "An Error happened, try again latter";
+  }
+}
+
+btn.addEventListener("click", getJoke);
