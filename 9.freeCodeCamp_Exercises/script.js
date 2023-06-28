9600c9816e0739851bd47f8fb3da30e4cd3c877d
Roger Acosta <rogeracosta359@gmail.com>
2023-06-28 18:33:25 -0500

    fibonacchi app

--- 9.freeCodeCamp_Exercises/script.js
+++ 9.freeCodeCamp_Exercises/script.js
@@ -27,12 +27,50 @@ standBtn.addEventListener("click", (e) => {
 
 // Extract Domain Name from an email
 const emailBtn = document.querySelector(".emailBtn");
-const domainResult = document.querySelector('.domainResult')
+const domainResult = document.querySelector(".domainResult");
 
 emailBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const emailInput = document.querySelector(".input-email").value;
 
   let getDomain = emailInput.substring(emailInput.indexOf("@") + 1);
-  domainResult.innerText = getDomain
+  domainResult.innerText = getDomain;
 });
+
+// Fibonacci with recursividad
+// 0,1,1,2,3,5,8
+// n = 0 -> 0
+// n = 1 -> 1
+// n = 2 -> 1 + 0
+
+// The simple way
+// const fibonacci = (n) => {
+//   // if (n === 0) return 0;
+//   // if (n === 1) return 1;
+//   if (n < 2) return n; // 2 + 1
+//   return fibonacci(n - 2) + fibonacci(n - 1)
+// };
+// // console.log(fibonacci(2))
+// console.log(fibonacci(8))
+
+// Fibonacci with FOR loop
+const fiboBtn = document.querySelector(".fiboBtn");
+const fiboResult = document.querySelector(".fiboResult");
+
+const fibonacci = (n) => {
+  const fib = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib[n];
+};
+
+fiboBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputFibo = document.querySelector(".input-fibo").value;
+
+  fiboResult.innerText = fibonacci(inputFibo);
+});
+
+
