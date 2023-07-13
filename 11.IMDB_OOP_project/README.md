c45a90a20f76f02e752380b30c91518c6162783c
Roger Acosta <rogeracosta359@gmail.com>
2023-07-12 20:25:46 -0500

    OOP project

--- 11.IMDB_OOP_project/README.md
+++ 11.IMDB_OOP_project/README.md
@@ -14,15 +14,15 @@
   ## - Valida que el título no rebase los 100 caracteres.
   ## - Valida que el director no rebase los 50 caracteres.
   ## - Valida que el año de estreno sea un número entero de 4 dígitos.
-  - Valida que el país o paises sea introducidos en forma de arreglo.
-  - Valida que los géneros sean introducidos en forma de arreglo.
-  - Valida que los géneros introducidos esten dentro de los géneros 
+  ## - Valida que el país o paises sea introducidos en forma de arreglo.
+  ## - Valida que los géneros sean introducidos en forma de arreglo.
+  ## - Valida que los géneros introducidos esten dentro de los géneros 
      aceptados*.
-  - Crea un método estático que devuelva los géneros aceptados*.
-  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
+  ## - Crea un método estático que devuelva los géneros aceptados*.
+  ## - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
     decimal de una posición.
-  - Crea un método que devuelva toda la ficha técnica de la película.
-  - Apartir de un arreglo con la información de 3 películas genera 3 
+  ## - Crea un método que devuelva toda la ficha técnica de la película.
+  ## - Apartir de un arreglo con la información de 3 películas genera 3 
     instancias de la clase de forma automatizada e imprime la ficha técnica 
     de cada película.
 
--- 11.IMDB_OOP_project/script.js
+++ 11.IMDB_OOP_project/script.js
@@ -10,7 +10,50 @@ class Movie {
     this.validateIMDBID(id);
     this.validateTitle(title);
     this.validateDirector(director);
-    this.validatePremiere(premiere)
+    this.validatePremiere(premiere);
+    this.validateCountry(country);
+    this.validateGenre(genre);
+    this.validateRating(rating);
+  }
+
+  static get genreList() {
+    return [
+      "Action",
+      "Adult",
+      "Adventure",
+      "Animation",
+      "Biography",
+      "Comedy",
+      "Crime",
+      "Documentary",
+      "Drama",
+      "Family",
+      "Fantasy",
+      "Film",
+      "Noir",
+      "Game-Show",
+      "History",
+      "Horror",
+      "Musical",
+      "Music",
+      "Mystery",
+      "News",
+      "Reality-TV",
+      "Romance",
+      "Sci-Fi",
+      "Short",
+      "Sport",
+      "Talk-Show",
+      "Thriller",
+      "War",
+      "Western",
+    ];
+  }
+
+  static aceptedGenres() {
+    return console.info(
+      `the acepted genres are: ${Movie.genreList.join(", ")}`
+    );
   }
 
   stringValidation(prop, value) {
@@ -30,7 +73,7 @@ class Movie {
     return true;
   }
 
-  arrayValidation(prop, value){
+  arrayValidation(prop, value) {
     if (!value) return console.warn(`${prop}: "${value}" is empty`);
     if (typeof value !== "number")
       return console.error(`${prop}: "${value}" entered, It's not a number`);
@@ -43,6 +86,23 @@ class Movie {
       return console.error(
         `${prop} ${value} excede el numero de caracteres permitidos ${length}`
       );
+    return true;
+  }
+
+  validateArray(prop, value) {
+    if (!value) return console.warn(`${prop} "${value}" is empty`);
+    if (!(value instanceof Array))
+      return console.error(`${prop} "${value}" inserted, Its not an Array`);
+
+    if (value.length === 0)
+      return console.error(`${prop} "${value}" dosent have data`);
+
+    for (let string of value) {
+      if (typeof string !== "string")
+        return console.error(
+          `The value "${string}}" inserted, Its not a string`
+        );
+    }
     return true;
   }
 
@@ -68,22 +128,98 @@ class Movie {
     }
   }
 
-  validatePremiere(premiere){
+  validatePremiere(premiere) {
     if (this.numberValidation("ano de estreno", premiere)) {
       if (!/^([0-9]){4}$/.test(premiere)) {
         return console.error(
           `The premiere: ${premiere} is not valid, it should be a number of 4 digits`
-        )}
+        );
+      }
     }
+  }
+
+  validateCountry(country) {
+    this.validateArray("Country", country);
+  }
+
+  validateGenre(genre) {
+    if (this.validateArray("Genre", genre)) {
+      for (let string of genre) {
+        if (!Movie.genreList.includes(string)) {
+          console.error(`genres incorrects "${genre.join(", ")}"`);
+          Movie.aceptedGenres();
+        }
+
+        // console.log(genre, Movie.genreList.includes(genre))
+      }
+    }
+  }
+
+  validateRating(rating) {
+    if (this.numberValidation("Rating", rating)) {
+      return rating < 0 || rating > 10
+        ? console.error(`Rating will be between 0 and 10`)
+        : (this.rating = rating.toFixed(1));
+    }
+  }
+
+  fichaTecnica() {
+    console.info(
+      `Ficha tecnica \n Title: "${this.title}"\n Director: "${
+        this.director
+      }"\n Premiere: "${this.premiere}"\n Country: "${this.country.join(
+        "-"
+      )}"\n Genres: "${this.genre.join(", ")}"\n Rating: "${
+        this.rating
+      }"\n IMDB Id: "${this.id}"`
+    );
   }
 }
 
-const rocky = new Movie({
-  id: "tt0439572",
-  title: "Titulo de la peli",
-  director: "Roger Acosta",
-  premiere: 2020,
-});
+// Movie.aceptedGenres()
+// const rocky = new Movie({
+//   id: "tt0439572",
+//   title: "Titulo de la peli",
+//   director: "Roger Acosta",
+//   premiere: 2020,
+//   country: ["Colombia", "Francia"],
+//   genre: ["War", "Sport"],
+//   rating: 7.78,
+// });
+
+// rocky.fichaTecnica();
+
+const myMovies = [
+  {
+    id: "tt0758758",
+    title: 'Into the Wild',
+    director: 'Sean Penn',
+    premiere: 2007,
+    country: ['USA'],
+    genre: ['Adventure', 'Biography', 'Drama'],
+    rating: 8.1
+  },
+  {
+    id: "tt0479143",
+    title: 'Rocky Balboa',
+    director: 'Sylvester Stallone',
+    premiere: 2006,
+    country: ['USA'],
+    genre: ['Action', 'Drama', 'Sport'],
+    rating: 7.1
+  },
+  {
+    id: "tt0468569",
+    title: 'The Dark Knight',
+    director: 'Christopher Nolan',
+    premiere: 2008,
+    country: ['USA', 'UK'],
+    genre: ['Adventure', 'Crime', 'Drama'],
+    rating: 9.0
+  }
+];
+
+myMovies.forEach(el => new Movie(el).fichaTecnica())
 
 /*
 Barbie ID = ls520251784
