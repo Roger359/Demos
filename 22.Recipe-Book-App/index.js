21fc0954edd93c904b8f298fd7336c0a1ed13296
Roger Acosta <rogeracosta359@gmail.com>
2023-08-02 11:06:25 -0500

    recipe book app

--- 22.Recipe-Book-App/index.js
+++ 22.Recipe-Book-App/index.js
@@ -1,4 +1,4 @@
-const API_KEY = "af7d44ccb9514eca97385fdfaea61854";
+const API_KEY = "715658beae7d4f2188ef63658cea1dde";
 const recipeList = document.getElementById("recipe-list");
 
 const getRecipes = async () => {
@@ -20,28 +20,33 @@ function displayRecipes(recipes) {
     recipeImage.src = recipe.image;
     recipeImage.alt = "Recipe Image";
 
+    const recipeTitle = document.createElement("h2");
+    recipeTitle.textContent = recipe.title;
 
-    const recipeTitle = document.createElement('h2')
-    recipeTitle.textContent = recipe.title
-
-    const recipeIngredients = document.createElement('p')
+    const recipeIngredients = document.createElement("p");
     recipeIngredients.innerHTML = `
-    <strong>Ingredients<strong>${recipe.extendedIngredients.map((ingredient)=> ingredient.original).join(', ')}
+    <strong>Ingredients<strong>${recipe.extendedIngredients
+      .map((ingredient) => ingredient.original)
+      .join(", ")}
     
-    `
+    `;
+
+    const recipeAnchor = document.createElement('a')
+    recipeAnchor.href = recipe.sourceUrl;
+    recipeAnchor.innerText = 'View Recipe'
 
     recipeItem.appendChild(recipeImage);
     recipeItem.appendChild(recipeTitle);
     recipeItem.appendChild(recipeIngredients);
+    recipeItem.appendChild(recipeAnchor);
     recipeList.appendChild(recipeItem);
   });
 }
 
 async function init() {
   const recipes = await getRecipes();
-
   displayRecipes(recipes);
-  console.log(recipes);
+ 
 }
 
 init();
