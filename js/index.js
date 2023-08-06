7ae430ce5e4415ca4584b81620ee9782cbe625cd
Roger Acosta <rogeracosta359@gmail.com>
2023-08-06 12:40:42 -0500

    clean script file

--- js/index.js
+++ js/index.js
@@ -1,19 +1,12 @@
 import { projects } from "./projects.js";
 
 const grid = document.querySelector(".grid");
-console.log(grid);
-// console.log(projects);
-
-console.log(projects);
-
-// function displayProjects(projects) {
 
 for (let i = 0; i < projects.length; i++) {
   console.log(projects[i]);
 
   const divContainer = document.createElement("div");
   divContainer.classList.add("project-container");
-
 
   const anchorContainer = document.createElement("a");
   const divInsideContainer = document.createElement("div");
@@ -35,23 +28,21 @@ for (let i = 0; i < projects.length; i++) {
   const h2Title = document.createElement("h2");
   h2Title.classList.add("h2-title");
   h2Title.textContent = projects[i].title;
-  
-  anchorTitle.appendChild(h2Title);
 
+  anchorTitle.appendChild(h2Title);
 
   const description = document.createElement("p");
   description.classList.add("description");
   description.textContent = projects[i].description;
 
-
   divTexts.appendChild(anchorTitle);
   divTexts.appendChild(description);
 
   const demoDiv = document.createElement("div");
-  demoDiv.classList.add('demo')
+  demoDiv.classList.add("demo");
   const demoAnchor = document.createElement("a");
   demoAnchor.textContent = "DEMO";
-  demoAnchor.target = '_blank'
+  demoAnchor.target = "_blank";
   demoAnchor.href = projects[i].src;
 
   demoDiv.appendChild(demoAnchor);
@@ -62,6 +53,3 @@ for (let i = 0; i < projects.length; i++) {
 
   grid.appendChild(divContainer);
 }
-// }
-
-// displayProjects();
